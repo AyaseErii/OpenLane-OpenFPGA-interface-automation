0cff542f5c24f4cc7f9eef9b0873d8724821a7b1
Cocogoat <70917894+AyaseErii@users.noreply.github.com>
2022-07-05 11:54:07 -0400

    Add files via upload

--- run_flow.sh
+++ run_flow.sh
@@ -1,5 +1,5 @@
 DESIGN_NAME2="cbx_1__0_ cbx_1__1_ cbx_1__2_ cby_0__1_ cby_1__1_ cby_2__1_ sb_0__0_ sb_0__1_ sb_0__2_ sb_1__0_ sb_1__1_ sb_1__2_ sb_2__0_ sb_2__1_ sb_2__2_ grid_clb"
-
+mkdir sub_macro_configs
 for dd in $DESIGN_NAME2
 do
     GRID_CLB_SIZE_X=250
@@ -19,7 +19,8 @@ do
     if [[ "$dd" = "grid_clb" ]]; then
     sed -i "/set ::env(CLOCK_PORT) \"prog_clk\"/a set ::env(FP_SIZING) absolute\nset ::env(DIE_AREA) \"0 0 $GRID_CLB_SIZE_X $GRID_CLB_SIZE_Y\"\nset ::env(DESIGN_IS_CORE) 0\nset ::env(FP_PDN_CORE_RING) 0\nset ::env(RT_MAX_LAYER) \"met4\"" config.txt
     mv config.txt config.tcl
-    cd ../..   
+    cd ../..
+    cp -r ./designs/$dd/config.tcl ./sub_macro_configs/$dd\_config.tcl   
     echo "\n==============Start the design flow of" $dd "=================\n"
     ./flow.tcl -design $dd
     cp -r ./designs/$dd/runs/*/results/final/gds/$dd.gds ./designs/fpga_core/gds
@@ -29,7 +30,8 @@ do
     elif [[ "$dd" = "cbx_1__0_" || "$dd" = "cbx_1__1_" || "$dd" = "cbx_1__2_" || "$dd" = "cby_0__1_" || "$dd" = "cby_1__1_" || "$dd" = "cby_2__1_" ]]; then
     sed -i "/set ::env(CLOCK_PORT) \"prog_clk\"/a set ::env(FP_SIZING) absolute\nset ::env(DIE_AREA) \"0 0 $CB_SIZE_X $CB_SIZE_Y\"\nset ::env(DESIGN_IS_CORE) 0\nset ::env(FP_PDN_CORE_RING) 0\nset ::env(RT_MAX_LAYER) \"met4\"" config.txt
     mv config.txt config.tcl
-    cd ../..  
+    cd ../.. 
+    cp -r ./designs/$dd/config.tcl ./sub_macro_configs/$dd\_config.tcl
     echo "\n==============Start the design flow of" $dd "=================\n"
     ./flow.tcl -design $dd
     cp -r ./designs/$dd/runs/*/results/final/gds/$dd.gds ./designs/fpga_core/gds
@@ -39,7 +41,8 @@ do
     else
     sed -i "/set ::env(CLOCK_PORT) \"prog_clk\"/a set ::env(FP_SIZING) absolute\nset ::env(DIE_AREA) \"0 0 $SB_SIZE_X $SB_SIZE_Y\"\nset ::env(DESIGN_IS_CORE) 0\nset ::env(FP_PDN_CORE_RING) 0\nset ::env(RT_MAX_LAYER) \"met4\"" config.txt
     mv config.txt config.tcl
-    cd ../.. 
+    cd ../..
+    cp -r ./designs/$dd/config.tcl ./sub_macro_configs/$dd\_config.tcl
     echo "\n==============Start the design flow of" $dd "=================\n"
     ./flow.tcl -design $dd
     cp -r ./designs/$dd/runs/*/results/final/gds/$dd.gds ./designs/fpga_core/gds
